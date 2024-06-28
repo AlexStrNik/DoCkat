ee604f1058579252e276d8f963e06e0144e6ebfe
alexstrnik <alex.str.nik@gmail.com>
2024-06-28 14:08:34 +0300

    Initial Commit

---
+++ DoCkat/DoCkatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoCkatApp.swift
+//  DoCkat
+//
+//  Created by Aleksandr Strizhnev on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoCkatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
